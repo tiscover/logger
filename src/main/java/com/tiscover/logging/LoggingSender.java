3c4020f37a20692ea9d699abbc09a3f4b3ee10be
Martin Anzinger <martin.anzinger@tiscover.com>
2017-05-24 13:25:24 +0200

    remove check for socket.isenabled

--- src/main/java/com/tiscover/logging/LoggingSender.java
+++ src/main/java/com/tiscover/logging/LoggingSender.java
@@ -3,38 +3,36 @@ package com.tiscover.logging;
 import java.util.TimerTask;
 
 public class LoggingSender extends TimerTask {
-	private final AbstractLoggingService service;
+    private final AbstractLoggingService service;
 
-	public LoggingSender(AbstractLoggingService service) {
-		this.service = service;
-	}
+    public LoggingSender(AbstractLoggingService service) {
+        this.service = service;
+    }
 
-	@Override
-	public void run() {
-		long dur = 0;
-		try {
-			if (!service.getSocket().isEnabled()) {
-				return;
-			}
+    @Override
+    public void run() {
+        long dur = 0;
+        try {
+            // do not check for service.getSocket().isEnabled() because if you
+            // do the maps are not cleaned in an error case
+            long tm = System.currentTimeMillis();
 
-			long tm = System.currentTimeMillis();
+            service.executeTimerTask();
 
-			service.executeTimerTask();
+            service.setLastTimerRun(System.currentTimeMillis());
+            dur = service.getLastTimerRun() - tm;
+        } catch (Exception e) {
 
-			service.setLastTimerRun(System.currentTimeMillis());
-			dur = service.getLastTimerRun() - tm;
-		} catch (Exception e) {
-
-		} finally {
-			if (!service.isStopped()) {
-				if (dur < service.getTimerIdleTime()) {
-					service.scheduleTimer(service.getTimerIdleTime() - dur);
-				} else {
-					service.scheduleTimer(service.getTimerShortIdleTime() - dur);
-				}
-			} else {
-				service.stopTimer();
-			}
-		}
-	}
+        } finally {
+            if (!service.isStopped()) {
+                if (dur < service.getTimerIdleTime()) {
+                    service.scheduleTimer(service.getTimerIdleTime() - dur);
+                } else {
+                    service.scheduleTimer(service.getTimerShortIdleTime() - dur);
+                }
+            } else {
+                service.stopTimer();
+            }
+        }
+    }
 }
